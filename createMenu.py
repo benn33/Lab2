d46421735cb532fd7f748cc98c952e9eb9e23829
Ben Rode <rodebe@mnstate.edu>
2016-02-24 12:48:22 -0600

    Created test for createmenu, added docus to creatMenu

--- createMenu.py
+++ createMenu.py
@@ -1,3 +1,6 @@
 def CreateMenu(listStrings):
+  '''
+  Takes a list of strings and prints them out one by one aper line.
+  '''
   for item in listStrings:
     print(item)
---
+++ testMenu.py
@@ -0,0 +1,6 @@
+from createMenu import *
+menu1 = ['1.Timmy','2.Billy','3.Joey']
+menu2 = ['1.Ben','2.Tom','3.Matt']
+
+CreateMenu(menu1)
+CreateMenu(menu2)
